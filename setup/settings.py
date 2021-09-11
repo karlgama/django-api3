ec86d1192e75c2945c2671c46d91fb6a3a152aac
kaique <kaiquesilva49@hotmail.com>
2021-09-11 11:55:14 -0300

    corrigido

--- setup/settings.py
+++ setup/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
-    'DEFAULT_PERMSSION_CLASSES': [
+    'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
         'rest_framework.permissions.DjangoModelPermissions',
         ],
